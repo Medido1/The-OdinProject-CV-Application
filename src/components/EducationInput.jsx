26936df1f032317fcc205f9a02ed13d9aa80ab49
Medido1 <medi1222@gmail.com>
2024-04-17 07:37:19 +0100

    Make the arrow icon change with a click

--- src/components/EducationInput.jsx
+++ src/components/EducationInput.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import educationIcon from '../assets/mortarboard.png';
 import arrowDown from '../assets/arrowDown.png';
 import arrowUp from '../assets/arrowUp.png';
 import '../styles/educationInput.css';
 
 export default function EducationInput() {
+  const [isShowForm, setIsShowForm] = useState(false);
+
+  function showForm() {
+    setIsShowForm(!isShowForm);
+  }
+
   return (
     <div className="form_container">
       <form className="education_form">
@@ -13,7 +19,9 @@ export default function EducationInput() {
             <img src={educationIcon} alt="education icon" className="icon big"/>
             <h2>Education</h2>
           </div>
-          <img src={arrowDown} alt="open menu img" className="icon" />
+          <img src={isShowForm ? arrowUp : arrowDown}
+          alt="open menu img" className="icon" 
+          onClick={showForm}/>
         </div>
       </form>
     </div>
