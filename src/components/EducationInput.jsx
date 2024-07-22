38f0ae1c709e3dfc24cf381e0c27b3dc31a9816e
Medido1 <medi1222@gmail.com>
2024-07-22 20:19:02 +0100

    Add arrow changable arrow button

--- src/components/EducationInput.jsx
+++ src/components/EducationInput.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import educationIcon from '../assets/icons/mortarboard.png'
+import React, { useState } from 'react';
+import educationIcon from '../assets/icons/mortarboard.png';
+import arrowUp from '../assets/icons/up-arrow.png';
+import arrowDown from '../assets/icons/down-arrow.png';
 
 export default function EducationInput() {
+  const [showForm, setShowForm] = useState(false);
+
+  function handleShowForm() {
+    setShowForm(!showForm);
+  }
   return (
     <form action="" className='education_form'>
       <h2>
         <img src={educationIcon} className='icon'/>
         Education
+        <img  src ={showForm ? arrowDown : arrowUp} className='icon'
+        onClick={handleShowForm}
+        />
       </h2>
     </form>
   )
