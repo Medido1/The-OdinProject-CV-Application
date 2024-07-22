9c1f3cfc49037bf9e32b116088428f22ee156bcf
Medido1 <medi1222@gmail.com>
2024-07-22 08:37:36 +0100

    Create personal details form

--- src/components/personalInput.jsx
+++ src/components/personalInput.jsx
@@ -8,6 +8,18 @@ export default function PersonalDetails() {
       <InputGrp id="firstName" 
       labeltext="First Name"
       type="text" placeholder="First Name"/>
+      <InputGrp id="lastName" 
+      labeltext="Last Name"
+      type="text" placeholder="Last Name"/>
+      <InputGrp id="email"
+      labeltext="Email" 
+      type="email" placeholder="example@email.com" />
+      <InputGrp id="phoneNumber" 
+      labeltext="Phone Number" 
+      type="tel" placeholder="Enter phone number"/>
+      <InputGrp id="address"
+      labeltext="Adress"
+      type="text" placeholder="City, Country"/>
     </form>
   )
 }
