efeb6ae8cde07c253860d0f492e352e793d2f07b
Medido1 <medi1222@gmail.com>
2024-07-22 11:09:03 +0100

    Add a header and a main for better structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className='main_header'>
+      <h1>My Cv App</h1>
+    </header>
+  )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import InputContainer from './InputContainer';
+
+export default function Main() {
+  return (
+    <main className='main'>
+      <InputContainer className="input_container"/>
+    </main>
+  )
+}
